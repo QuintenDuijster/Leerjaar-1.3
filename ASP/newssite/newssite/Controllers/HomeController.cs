e982eb48bba697cf85f642cc4dcf7774483c086d
Entropire <36831@ma-web.nl>
2024-02-27 21:46:44 +0100

    dont now what this is but oke here it is

---
+++ ASP/newssite/newssite/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using newssite.Models;
+using System.Diagnostics;
+using System.Text.Json;
+
+namespace newssite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private List<NewsItem> newsItems = new List<NewsItem>();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+
+            string json = System.IO.File.ReadAllText("data/news.json");
+            newsItems = JsonSerializer.Deserialize<List<NewsItem>>(json);
+        }
+
+        public IActionResult Index()
+        {
+            return View(newsItems);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
