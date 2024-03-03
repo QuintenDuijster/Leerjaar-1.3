d941502d37252295926d7c29f566cc4641e8e568
Quinten <146432390+QuintenDuijster@users.noreply.github.com>
2024-03-03 15:31:57 +0100

    opdracht 06

--- ASP/newssite/newssite/Controllers/HomeController.cs
+++ ASP/newssite/newssite/Controllers/HomeController.cs
@@ -29,6 +29,11 @@ namespace newssite.Controllers
             return View();
         }
 
+        public IActionResult Games()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
